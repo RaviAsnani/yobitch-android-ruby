d2717a08e963b2e4075a3540af85e18a347fa27f
Mayank Jain <mayank.jain@tradus.com>
2014-07-08 21:58:51 +0530

    Working right drawer

--- src/main_activity.rb
+++ src/main_activity.rb
@@ -2,19 +2,32 @@ require 'ruboto/widget'
 require 'ruboto/util/toast'
 
 java_import 'com.pixate.freestyle.PixateFreestyle'
+java_import 'android.support.v4.widget.DrawerLayout'
+
 
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
 
 class MainActivity
 
+  attr_accessor :drawer_layout, :abuse_selection_list
+
   # Entry point into the app
   def onCreate(bundle)
     super
     set_title "Yo! B*tch!"
+    init_activity()
+  end
+
+
+  def init_activity
     PixateFreestyle.init(self)
-    
     setContentView($package.R.layout.main)
+
+    @drawer_layout = find_view_by_id($package.R::id::drawer_layout)
+    @abuse_selection_list = find_view_by_id($package.R::id::abuse_selection_list)
+    
+    @drawer_layout.open_drawer(@abuse_selection_list)
   end
 
 end
