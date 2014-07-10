2b6baebe2fb92e8c47d5d79df51113766c9d20c9
Mayank Jain <mayank.jain@tradus.com>
2014-07-10 12:17:16 +0530

    working http post

--- src/app/common/net.rb
+++ src/app/common/net.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require "uri"
+
 require "app/boot"
 
 module Net
@@ -9,14 +11,44 @@ module Net
     t = Thread.start do
       begin 
         response = Net::HTTP.get(domain, path)
+
         # Build error handling mechanism
         json_obj = JSON.parse(response)
-        block.call(json_obj)
+        block.call(json_obj)  
       rescue Exception
         log_exception(:net_get, $!)
       end
     end
     t.join
+  end
+
+
+
+  def post(domain, path, json_string, &block)
+    t = Thread.start do
+      begin
+        uri = URI.parse(SCHEME + domain + path)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.body = json_string
+        request["Content-Type"] = "application/json"
+        response = http.request(request)
+
+        # Build error handling mechanism
+        json_obj = JSON.parse(response.body)
+        block.call(json_obj)  
+      rescue Exception
+        log_exception(:net_post, $!)
+      end
+    end
+    t.join
+  end
+
+
+  private
+
+  def parse_json_string(json_string, &block)
+  
   end
 
 
--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -12,12 +12,19 @@ class User
 
 
   def save(&block)
-    Logger.d "Stuff!"
+    Logger.d "Saving user"
+
     # TODO - change to post in actual network call
     get(BASE_SERVER, USER_GET) do |user_json|
       Logger.d user_json["messages"].first["abuse"]
       block.call(user_json)
     end
+
+    # post("yobitch.me", "/api/v1/users", '{"user": {"name": "Ravi Asnani","email": "asnafam2@gmail.com","gcm_token":"foo"}}') do |json_user|
+    #   Logger.d("**********************")
+    #   Logger.d(json_user.to_s)
+    #   Logger.d("**********************")
+    # end
   end    
 
 end
