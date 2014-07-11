adecbdf8737770ae79af5b23665a93a70e11f1e3
Mayank Jain <mayank.jain@tradus.com>
2014-07-11 07:57:24 +0530

    Temp moved to dev url's

--- src/app/config.rb
+++ src/app/config.rb
@@ -1,7 +1,8 @@
 DEBUG = true
 SCHEME = "http://"
-#BASE_SERVER = "192.168.2.100"
-BASE_SERVER = "yobitch.me"
+BASE_SERVER = "192.168.43.186"
+#BASE_SERVER = "yobitch.me"
 USER_GET = "/user_post.json"
-USER_SAVE = "/api/v1/users"
+#USER_SAVE = "/api/v1/users"
+USER_SAVE = "/user_post.json"
 USER_POST = "/api/v1/users/send_message"
